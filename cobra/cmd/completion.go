c73f21f56dc625424a3f379c5b84e0e0db838e97
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-24 11:06:15 -0600

    update cobra default completion

--- cobra/cmd/completion.go
+++ cobra/cmd/completion.go
@@ -17,6 +17,7 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -31,54 +32,54 @@ var ()
 
 // completionCmd
 var completionCmd = &cobra.Command{
-	Use:    "completion",
+	Use:    "completion [bash|zsh|fish|powershell]",
 	Hidden: true,
 	Short:  "Generate completion script.",
-	Long: `To load completions:
+	Long: fmt.Sprintf(`To load completions:
 
 Bash:
 
-$ source <(COMMAND completion bash)
+  $ source <(%[1]s completion bash)
 
-# To load completions for each session, execute once:
-Linux:
- $ COMMAND completion bash > /etc/bash_completion.d/COMMAND
-MacOS:
- $ COMMAND completion bash > /usr/local/etc/bash_completion.d/COMMAND
+  # To load completions for each session, execute once:
+  # Linux:
+  $ %[1]s completion bash > /etc/bash_completion.d/%[1]s
+  # macOS:
+  $ %[1]s completion bash > $(brew --prefix)/etc/bash_completion.d/%[1]s
 
 Zsh:
 
-# If shell completion is not already enabled in your environment you will need
-# to enable it.  You can execute the following once:
+  # If shell completion is not already enabled in your environment,
+  # you will need to enable it.  You can execute the following once:
 
-$ echo "autoload -U compinit; compinit" >> ~/.zshrc
+  $ echo "autoload -U compinit; compinit" >> ~/.zshrc
 
-# To load completions for each session, execute once:
-$ COMMAND completion zsh > "${fpath[1]}/_COMMAND"
+  # To load completions for each session, execute once:
+  $ %[1]s completion zsh > "${fpath[1]}/_%[1]s"
 
-# You will need to start a new shell for this setup to take effect.
+  # You will need to start a new shell for this setup to take effect.
 
-Fish:
+fish:
 
-$ COMMAND completion fish | source
+  $ %[1]s completion fish | source
 
-# To load completions for each session, execute once:
-$ COMMAND completion fish > ~/.config/fish/completions/COMMAND.fish
+  # To load completions for each session, execute once:
+  $ %[1]s completion fish > ~/.config/fish/completions/%[1]s.fish
 
-Powershell:
+PowerShell:
 
-PS> COMMAND completion powershell | Out-String | Invoke-Expression
+  PS> %[1]s completion powershell | Out-String | Invoke-Expression
 
-# To load completions for every new session, run:
-PS> COMMAND completion powershell > COMMAND.ps1
-# and source this file from your powershell profile.
-`,
+  # To load completions for every new session, run:
+  PS> %[1]s completion powershell > %[1]s.ps1
+  # and source this file from your PowerShell profile.
+`, cmd.Root().Name()),
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	DisableFlagsInUseLine: true,
 	ValidArgs:             []string{"bash", "zsh", "fish", "powershell"},
-	Args:                  cobra.OnlyValidArgs,
+	Args:                  cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
 
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -91,7 +92,7 @@ PS> COMMAND completion powershell > COMMAND.ps1
 		case "fish":
 			κ.Root().GenFishCompletion(os.Stdout, true)
 		case "powershell":
-			κ.Root().GenPowerShellCompletion(os.Stdout)
+			κ.Root().GenPowerShellCompletionWithDesc(os.Stdout)
 		}
 	},
 }
