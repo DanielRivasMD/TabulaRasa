83b9670fda56c8710e787598f3bf71c1bea40010
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-25 16:20:26 -0600

    overhall copy & replace logic

--- cmd/execCmd.go
+++ cmd/execCmd.go
@@ -5,49 +5,133 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
-	"bytes"
-	"os/exec"
+	"bufio"
+	"io"
+	"log"
+	"os"
+	"strconv"
 	"strings"
-
-	"github.com/labstack/gommon/color"
+	"time"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// execute shell command
-func deploy(path, repo, author_email string) {
+// copy file
+func copyFile(orig, dest string) {
+	// handle original
+	origFile, ε := os.Open(orig)
+	if ε != nil {
+		log.Fatal(ε)
+	}
+	defer origFile.Close()
 
-	// declare
-	tool := strings.ToLower(repo)
-
-	// buffers
-	var stdout bytes.Buffer
-	var stderr bytes.Buffer
+	// handle destiny
+	destFile, ε := os.Create(dest)
+	if ε != nil {
+		log.Fatal(ε)
+	}
+	defer destFile.Close()
 
 	// copy
-	copyShell := findHome() + configDir + "/" + "copy.sh"
-	copyCmd := exec.Command(copyShell, path)
-	copyCmd.Stdout = &stdout
-	copyCmd.Stderr = &stderr
-	_ = copyCmd.Run()
+	_, ε = io.Copy(destFile, origFile)
+	if ε == nil {
+		origInfo, ε := os.Stat(orig)
+		if ε != nil {
+			ε = os.Chmod(dest, origInfo.Mode())
+		}
+	}
+}
 
-	// replace
-	replaceShell := findHome() + configDir + "/" + "replace.sh"
-	replaceCmd := exec.Command(replaceShell, repo, tool, author_email)
-	replaceCmd.Stdout = &stdout
-	replaceCmd.Stderr = &stderr
-	_ = replaceCmd.Run()
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
-	// stdout
-	if stdout.String() != "" {
-		color.Println(color.Cyan(stdout.String(), color.B))
+// copy dir
+func copyDir(orig, dest string) {
+
+	// original properties
+	origInfo, ε := os.Stat(orig)
+	if ε != nil {
+		log.Fatal(ε)
 	}
 
-	// stderr
-	if stderr.String() != "" {
-		color.Println(color.Red(stderr.String(), color.B))
+	// create destiny dir
+	ε = os.MkdirAll(dest, origInfo.Mode())
+	if ε != nil {
+		log.Fatal(ε)
 	}
 
+	// original files
+	dir, _ := os.Open(orig)
+	objs, ε := dir.Readdir(-1)
+
+	// iterate originals
+	for _, obj := range objs {
+
+		// pointers
+		origPointer := orig + "/" + obj.Name()
+		destPointer := dest + "/" + obj.Name()
+
+		if obj.IsDir() {
+			// create dirs recursive
+			copyDir(origPointer, destPointer)
+		} else {
+			// copy
+			copyFile(origPointer, destPointer)
+			// replace
+			replace(destPointer)
+		}
+	}
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// replace
+func replace(fullpath string) {
+	// open reader
+	fread, ε := os.Open(fullpath)
+	if ε != nil {
+		log.Fatal(ε)
+	}
+	defer fread.Close()
+
+	// open writer
+	fwrite, ε := os.OpenFile(fullpath, os.O_WRONLY|os.O_CREATE, 0666)
+	if ε != nil {
+		panic(ε)
+	}
+	defer fwrite.Close()
+
+	// declare writer
+	ϖ := bufio.NewWriter(fwrite)
+
+	// read file
+	scanner := bufio.NewScanner(fread)
+
+	// scan
+	for scanner.Scan() {
+
+		// preallocate
+		toPrint := scanner.Text()
+
+		// replace
+		toPrint = strings.Replace(toPrint, "YEAR", strconv.Itoa(time.Now().Year()), -1)
+		toPrint = strings.Replace(toPrint, "REPOSITORY", repo, -1)
+		toPrint = strings.Replace(toPrint, "TOOL", strings.ToLower(repo), -1)
+		toPrint = strings.Replace(toPrint, "AUTHOR_EMAIL", author_email, -1)
+
+		// write
+		_, ε = ϖ.WriteString(toPrint + "\n")
+		if ε != nil {
+			log.Fatal(ε)
+		}
+
+	}
+
+	if ε := scanner.Err(); ε != nil {
+		log.Fatal(ε)
+	}
+
+	// flush writer
+	ϖ.Flush()
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
