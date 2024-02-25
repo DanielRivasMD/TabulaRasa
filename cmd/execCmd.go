99077b160f3d1600cca750343b38976c7f6f00ba
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-25 07:48:40 -0600

    add exec command

---
+++ cmd/execCmd.go
@@ -0,0 +1,49 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package cmd
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"bytes"
+	"os/exec"
+
+	"github.com/labstack/gommon/color"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute shell command
+func deployCmd(path, repo, author_email string) {
+
+	// buffers
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	// copy
+	copyShell := findHome() + gobin + "/" + "cobra" + "/" + "copy.sh"
+	copyCmd := exec.Command(copyShell, path)
+	copyCmd.Stdout = &stdout
+	copyCmd.Stderr = &stderr
+	_ = copyCmd.Run()
+
+	println(copyShell)
+
+	// replace
+	replaceShell := findHome() + gobin + "/" + "cobra" + "/" + "replace.sh"
+	replaceCmd := exec.Command(replaceShell, repo, author_email)
+	replaceCmd.Stdout = &stdout
+	replaceCmd.Stderr = &stderr
+	_ = replaceCmd.Run()
+
+	// stdout
+	color.Println(color.Cyan(stdout.String(), color.B))
+
+	// stderr
+	if stderr.String() != "" {
+		color.Println(color.Red(stderr.String(), color.B))
+	}
+
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
