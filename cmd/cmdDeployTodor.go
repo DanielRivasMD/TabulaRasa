15e8c63a396bd2ef488a49c46adcbe998035b3c0
DanielRivasMD <danielrivasmd@gmail.com>
2024-02-28 07:45:18 -0600

    refactor subcmd todor

---
+++ cmd/cmdDeployTodor.go
@@ -0,0 +1,45 @@
+/*
+Copyright © 2024 Daniel Rivas <danielrivasmd@gmail.com>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// todorCmd represents the todor command
+var todorCmd = &cobra.Command{
+	Use:   "todor",
+	Short: "",
+	Long: `.`,
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Run: func(cmd *cobra.Command, args []string) {
+		copyFile(findHome() + todorDir + "/" + todorconfig, path + "/" + todor)
+	},
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// execute prior main
+func init() {
+	deployCmd.AddCommand(todorCmd)
+
+	// flags
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
