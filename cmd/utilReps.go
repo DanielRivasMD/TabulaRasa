2897476e4d80e21794ee3f2fd5f70dd2351e2f77
DanielRivasMD <danielrivasmd@gmail.com>
2024-10-21 15:55:27 -0600

    add replace cobra cmd

--- cmd/utilReps.go
+++ cmd/utilReps.go
@@ -25,6 +25,21 @@ func repsCobra() []rep {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
+// bind cobra replace values
+func replacerCobraCmd() []rep {
+	out := make([]rep, 7)
+	out[0] = rep{old: "YEAR", new: strconv.Itoa(time.Now().Year())}
+	out[1] = rep{old: "REPOSITORY", new: repo}
+	out[2] = rep{old: "TOOL", new: strings.ToLower(repo)}
+	out[3] = rep{old: "AUTHOR", new: author}
+	out[4] = rep{old: "EMAIL", new: email}
+	out[5] = rep{old: "CHILD", new: strings.ToLower(child)}
+	out[6] = rep{old: "PARENT", new: strings.ToLower(parent)}
+	return out
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
 // bind deploy just replace values
 func repsDeployJust() []rep {
 	out := make([]rep, 3)
