57f4dd8c87aef529cc30a5e77db25b0fa74ca9dd
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-24 10:45:00 -0600

    add detect license util

---
+++ cmd/utilDetectLicense.go
@@ -0,0 +1,41 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package cmd
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"os"
+	"strings"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// detectLicense identifies license type
+func detectLicense(filename string) (string, error) {
+	data, err := os.ReadFile(filename)
+	checkErr(err)
+
+	content := strings.ToLower(string(data))
+
+	// mapping common license identifiers
+	licenseKeywords := map[string]string{
+		"mit license": "MIT",
+		"apache license": "Apache-2.0",
+		"gnu general public license": "GPL",
+		"bsd license": "BSD",
+		"mozilla public license": "MPL",
+		"creative commons": "CC",
+		"eclipse public license": "EPL",
+	}
+
+	for keyword, licenseType := range licenseKeywords {
+		if strings.Contains(content, keyword) {
+			return licenseType, nil
+		}
+	}
+
+	return "Unknown License", nil
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
