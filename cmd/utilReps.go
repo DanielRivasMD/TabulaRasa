601fef65db183ed7bf2f166f90bc1eecdcc4d531
Daniel Rivas <danielrivasmd@gmail.com>
2025-04-24 10:44:48 -0600

    add replce deploy readme util

--- cmd/utilReps.go
+++ cmd/utilReps.go
@@ -51,3 +51,23 @@ func replaceDeployJust() []rep {
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// bind deploy readme replace values
+func replaceDeployReadme() []rep {
+
+	if fileExist(path + "/" + "LICENSE") && license == "" {
+		license, _ = detectLicense(path + "/" + "LICENSE")
+	}
+
+	Ω := make([]rep, 7)
+	Ω[0] = rep{old: "LANG", new: lang.selected[0]}
+	Ω[1] = rep{old: "OVERVIEW", new: description}
+	Ω[2] = rep{old: "REPOSITORY", new: repo}
+	Ω[3] = rep{old: "USER", new: user}
+	Ω[4] = rep{old: "AUTHOR", new: author}
+	Ω[5] = rep{old: "YEAR", new: strconv.Itoa(time.Now().Year())}
+	Ω[6] = rep{old: "LICENSETYPE", new: license}
+	return Ω
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
