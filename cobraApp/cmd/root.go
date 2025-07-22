fb155b7e281cdeeb3371d9cbf75a65d9b87f8b62
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-22 16:44:42 +0200

    update cobra root template

--- cobraApp/cmd/root.go
+++ cobraApp/cmd/root.go
@@ -17,38 +17,29 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
 	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Global declarations (reserved for future variables)
-var (
-// Add any global variables here if necessary.
-)
+var ()
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// rootCmd defines the base command when called without any subcommands.
 var rootCmd = &cobra.Command{
 	Use:   "TOOL",
 	Short: "A brief description of your tool", // Customize with your actual tool description.
-	Long: chalk.Green.Color(chalk.Bold.TextStyle("AUTHOR")) +
-		chalk.Dim.TextStyle(chalk.Italic.TextStyle("EMAIL")) + "\n\n" +
-		chalk.Cyan.Color("TOOL") + chalk.Blue.Color("\n\n"),
-	Example: "\n" + chalk.Cyan.Color("TOOL") + " help",
+	Long: chalk.Green.Color(chalk.Bold.TextStyle("AUTHOR ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("EMAIL")) + `
+
+` + chalk.Blue.Color("TOOL") + `
+`,
+	Example: chalk.White.Color("TOOL") + ` ` + chalk.Bold.TextStyle(chalk.White.Color("help")),
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// Execute is the entry point for executing the command.
-// It wraps the root command execution and handles any errors using Horus's checkErr function.
 func Execute() {
 	err := rootCmd.Execute()
 	horus.CheckErr(err)
@@ -56,54 +47,7 @@ func Execute() {
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// initializeConfig sets up configuration using Viper.
-// It also leverages the Domovoi library for any additional configuration management.
-func initializeConfig(cmd *cobra.Command, configPath string, configName string) error {
-	// Create a new Viper instance for configuration management.
-	vConfig := viper.New()
-
-	// Set the path and name of the configuration file.
-	vConfig.AddConfigPath(configPath)
-	vConfig.SetConfigName(configName)
-
-	// Attempt to read the configuration file.
-	err := vConfig.ReadInConfig()
-	if err != nil {
-		// If the config file is not found, that's acceptable.
-		_, notFound := err.(viper.ConfigFileNotFoundError)
-		if !notFound {
-			// Return the error for any other issue.
-			return err
-		}
-	}
-
-	// Bind command flags with configuration values.
-	bindFlags(cmd, vConfig)
-
-	return nil
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// bindFlags synchronizes each Cobra flag with the corresponding Viper configuration value.
-// If the flag is unset and a configuration value is available, the flag is updated.
-func bindFlags(cmd *cobra.Command, vConfig *viper.Viper) {
-	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		// If the flag wasn't explicitly set but has a value in the config, apply that value.
-		if !flag.Changed && vConfig.IsSet(flag.Name) {
-			value := vConfig.Get(flag.Name)
-			cmd.Flags().Set(flag.Name, fmt.Sprintf("%v", value))
-		}
-	})
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// Execute prior main.
-// init registers persistent flags and performs additional initialization tasks.
 func init() {
-	// Set up persistent flags.
-	// rootCmd.PersistentFlags().StringVar(&configFile, "config", "", "config file (default is $HOME/.tool.yaml)")
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
