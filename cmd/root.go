d4a0a75636435a32af6cd9218d826aa073867113
Daniel Rivas <danielrivasmd@gmail.com>
2025-07-23 11:45:36 +0200

    update root

--- cmd/root.go
+++ cmd/root.go
@@ -17,12 +17,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package cmd
 
 import (
-	"fmt"
-
 	"github.com/DanielRivasMD/horus"
 	"github.com/spf13/cobra"
-	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
 	"github.com/ttacon/chalk"
 )
 
@@ -67,7 +63,7 @@ var rootCmd = &cobra.Command{
 	Short: "Provide a canvas to write on",
 	Long: chalk.Green.Color(chalk.Bold.TextStyle("Daniel Rivas ")) + chalk.Dim.TextStyle(chalk.Italic.TextStyle("<danielrivasmd@gmail.com>")) + `
 
-` + chalk.Blue.Color("tab") + `, provide a set of templates to facilite software deployment
+` + chalk.Blue.Color("tabularasa") + `, provide a set of templates to facilite software deployment
 `,
 	Example: chalk.White.Color("tab") + ` ` + chalk.Bold.TextStyle(chalk.White.Color("help")),
 
@@ -80,51 +76,7 @@ var rootCmd = &cobra.Command{
 // Execute is the entry point for executing the command.
 // It wraps the root command execution and handles any errors using Horus's checkErr function.
 func Execute() {
-	err := rootCmd.Execute()
-	horus.CheckErr(err)
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// initializeConfig sets up configuration using Viper.
-// It also leverages the Domovoi library for any additional configuration management.
-func initializeConfig(cmd *cobra.Command, configPath string, configName string) error {
-	// Create a new Viper instance for configuration management.
-	vConfig := viper.New()
-
-	// Set the path and name of the configuration file.
-	vConfig.AddConfigPath(configPath)
-	vConfig.SetConfigName(configName)
-
-	// Attempt to read the configuration file.
-	err := vConfig.ReadInConfig()
-	if err != nil {
-		// If the config file is not found, that's acceptable.
-		_, notFound := err.(viper.ConfigFileNotFoundError)
-		if !notFound {
-			// Return the error for any other issue.
-			return err
-		}
-	}
-
-	// Bind command flags with configuration values.
-	bindFlags(cmd, vConfig)
-
-	return nil
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-// bindFlags synchronizes each Cobra flag with the corresponding Viper configuration value.
-// If the flag is unset and a configuration value is available, the flag is updated.
-func bindFlags(cmd *cobra.Command, vConfig *viper.Viper) {
-	cmd.Flags().VisitAll(func(flag *pflag.Flag) {
-		// If the flag wasn't explicitly set but has a value in the config, apply that value.
-		if !flag.Changed && vConfig.IsSet(flag.Name) {
-			value := vConfig.Get(flag.Name)
-			cmd.Flags().Set(flag.Name, fmt.Sprintf("%v", value))
-		}
-	})
+	horus.CheckErr(rootCmd.Execute())
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -132,17 +84,17 @@ func bindFlags(cmd *cobra.Command, vConfig *viper.Viper) {
 func init() {
 	rootCmd.PersistentFlags().BoolVarP(&verbose, "verbose", "v", false, "Enable verbose diagnostic output")
 
-	// appCmd.Flags().StringVar(&projectPath, "path", "", "Target directory for your new app")
+	// appCmd.Flags().StringVar(&projectPath, "path", "", "")
 	// appCmd.Flags().StringVar(&repoName, "repo", "", "Name of the repository (and Go module)")
 	// appCmd.Flags().StringVar(&authorName, "author", "", "Author’s full name")
 	// appCmd.Flags().StringVar(&authorEmail, "email", "", "Author’s email address")
 	// appCmd.Flags().StringVar(&userName, "username", "", "GitHub username")
 
-	rootCmd.PersistentFlags().StringVarP(&path, "path", "p", ".", "Path to deploy")
+	rootCmd.PersistentFlags().StringVarP(&path, "path", "p", ".", "Target directory for new app")
 	rootCmd.PersistentFlags().StringVarP(&repo, "repo", "r", "", "Repository name")
-	rootCmd.PersistentFlags().StringVarP(&author, "author", "a", "Daniel Rivas", "Provide author")
-	rootCmd.PersistentFlags().StringVarP(&email, "email", "e", "<danielrivasmd@gmail.com>", "Provide email")
-	rootCmd.PersistentFlags().StringVarP(&user, "user", "u", "DanielRivasMD", "Provide GitHub username")
+	rootCmd.PersistentFlags().StringVarP(&author, "author", "a", "Daniel Rivas", "Author name")
+	rootCmd.PersistentFlags().StringVarP(&email, "email", "e", "<danielrivasmd@gmail.com>", "Author email")
+	rootCmd.PersistentFlags().StringVarP(&user, "user", "u", "DanielRivasMD", "GitHub username")
 
 	rootCmd.MarkFlagRequired("path")
 	rootCmd.MarkFlagRequired("repo")
