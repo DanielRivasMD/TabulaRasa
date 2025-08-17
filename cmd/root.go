ec21f6b87d23a9c2bbc4b06ea2162a62855101e5
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-17 08:40:24 +0200

    consolidate types root

--- cmd/root.go
+++ cmd/root.go
@@ -27,8 +27,8 @@ import (
 
 // rootCmd
 var rootCmd = &cobra.Command{
-	Use:   "tab",
-	Long: helpRoot,
+	Use:     "tab",
+	Long:    helpRoot,
 	Example: exampleRoot,
 }
 
@@ -91,6 +91,52 @@ func init() {
 	_ = rootCmd.MarkFlagRequired("path")
 	_ = rootCmd.MarkFlagRequired("repo")
 
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// replace values
+type rep struct {
+	old string
+	new string
+}
+
+// CopyParams holds information needed to copy a template tree and apply string replacements.
+type CopyParams struct {
+	Orig  string   // source template directory
+	Dest  string   // destination directory
+	Files []string // specific files to copy (nil = all)
+	Reps  []rep    // replacement rules
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// newCopyParams creates a fresh CopyParams struct.
+func newCopyParams(orig, dest string) CopyParams {
+	return CopyParams{
+		Orig:  orig,
+		Dest:  dest,
+		Files: []string{},
+		Reps:  []rep{},
+	}
+}
+
+// withReplacements initializes a CopyParams with a set of replacements.
+func withReplacements(orig string, reps []rep) CopyParams {
+	return CopyParams{
+		Orig: orig,
+		Reps: reps,
+	}
+}
+
+// cloneCopyParams makes a shallow copy of an existing CopyParams.
+func cloneCopyParams(src CopyParams) CopyParams {
+	return CopyParams{
+		Orig:  src.Orig,
+		Dest:  src.Dest,
+		Files: src.Files,
+		Reps:  src.Reps,
+	}
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
