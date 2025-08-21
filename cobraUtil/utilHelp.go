2d68348d61e96c2a4443b81c763b39cd88fd9e17
Daniel Rivas <danielrivasmd@gmail.com>
2025-08-21 22:30:36 +0200

    update help util template

--- cobraUtil/utilHelp.go
+++ cobraUtil/utilHelp.go
@@ -33,51 +33,7 @@ func formatHelp(author, email, desc string) string {
 var helpRoot = formatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
-	"Master of daemons",
-)
-
-var helpInvoke = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"Spawn daemon process for the specified directory & execute the configured script on change\n"+
-		"Metadata is persistent for summoning the daemon",
-)
-
-var helpSlay = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"Gracefully stop alive daemons, removing their metadata and logs to allow clean reinvocation",
-)
-
-var helpTally = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"List all daemons invoked, showing group, PID, start time, and current status",
-)
-
-var helpFreeze = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"Pause daemon execution using SIGSTOP, until resumed manually",
-)
-
-var helpSummon = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"Display daemon log output\n"+
-		"Pass "+chalk.Italic.TextStyle("--follow")+" to stream in real time",
-)
-
-var helpInstall = formatHelp(
-	"Daniel Rivas ",
-	"<danielrivasmd@gmail.com>",
-	"Install lilith config",
-)
-
-var helpRekindle = formatHelp(
-	"Daniel Rivas",
-	"danielrivasmd@gmail.com",
-	"Restart daemons in limbo using persisted metadata",
+	"",
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
