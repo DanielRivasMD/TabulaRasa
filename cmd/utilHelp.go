657be80ccd9fc1d554245b7b4f79121e5ae23ffc
Daniel Rivas <danielrivasmd@gmail.com>
2025-09-10 10:00:02 +0200

    update help util

--- cmd/utilHelp.go
+++ cmd/utilHelp.go
@@ -5,80 +5,61 @@ package cmd
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 import (
+	"github.com/DanielRivasMD/domovoi"
 	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
-// formatHelp produces the “help” header + description.
-//
-//	author: name, e.g. "Daniel Rivas"
-//	email:  email, e.g. "danielrivasmd@gmail.com"
-//	desc:   the multi‐line description, "\n"-separated.
-func formatHelp(author, email, desc string) string {
-	header := chalk.Bold.TextStyle(
-		chalk.Green.Color(author+" "),
-	) +
-		chalk.Dim.TextStyle(
-			chalk.Italic.TextStyle("<"+email+">"),
-		)
-
-	// prefix two newlines to your desc, chalk it cyan + dim it
-	body := "\n\n" + desc
-	return header + chalk.Dim.TextStyle(chalk.Cyan.Color(body))
-}
-
-////////////////////////////////////////////////////////////////////////////////////////////////////
-
-var helpRoot = formatHelp(
+var helpRoot = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Provide a set of templates to facilite software deployment",
 )
 
-var helpCobra = formatHelp(
+var helpCobra = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Enables the creation of cobra applications using predefined templates for rapid development",
 )
 
-var helpCobraApp = formatHelp(
+var helpCobraApp = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
-	"Construct "+chalk.Italic.TextStyle("cobra") + " apps from predefined templates",
+	"Construct "+chalk.Italic.TextStyle("cobra")+" apps from predefined templates",
 )
 
-var helpCobraCmd = formatHelp(
+var helpCobraCmd = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
-	"Construct "+chalk.Italic.TextStyle("cobra") + " apps from predefined templates",
+	"Construct "+chalk.Italic.TextStyle("cobra")+" apps from predefined templates",
 )
 
-var helpCobraUtil = formatHelp(
+var helpCobraUtil = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Deploy a utility from predefined templates",
 )
 
-var helpDeploy = formatHelp(
+var helpDeploy = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Deploy selected config templates into your project",
 )
 
-var helpDeployJust = formatHelp(
+var helpDeployJust = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Deploy justfile & language‐specific configs",
 )
 
-var helpDeployReadme = formatHelp(
+var helpDeployReadme = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Deploy readme template with sections: overview, install/dev guides, usage & FAQ snippets",
 )
 
-var helpDeployTodor = formatHelp(
+var helpDeployTodor = domovoi.FormatHelp(
 	"Daniel Rivas",
 	"danielrivasmd@gmail.com",
 	"Deploy top-level todor config",
