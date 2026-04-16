b7516bad7bb5f4130ee3f098d59cd71c3d65eb45
Daniel Rivas <danielrivasmd@gmail.com>
2026-04-16 15:50:57 +0300

    add avicenna root skeleton

---
+++ skeleton/avicenna/root.jl
@@ -0,0 +1,21 @@
+####################################################################################################
+
+module XXX_ROOT_XXX
+
+####################################################################################################
+
+include("util/util_XXX_ROOT_LOWERCASE_XXX.jl")
+include("flow/flow_XXX_ROOT_LOWERCASE_XXX.jl")
+include("inter/cli/cli_XXX_ROOT_LOWERCASE_XXX.jl")
+include("inter/repl/repl_XXX_ROOT_LOWERCASE_XXX.jl")
+include("inter/doc/doc_XXX_ROOT_LOWERCASE_XXX.jl")
+
+####################################################################################################
+
+export XXX_ROOT_XXXCore, XXX_ROOT_XXXFlow, XXX_ROOT_XXXCLI, XXX_ROOT_XXXREPL
+
+####################################################################################################
+
+end
+
+####################################################################################################
