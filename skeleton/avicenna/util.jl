500bbc0d8b89eba1732b22e5ddb4ade50bdf8f94
Daniel Rivas <danielrivasmd@gmail.com>
2026-05-05 13:19:31 +0200

    update util avicenna

--- skeleton/avicenna/util.jl
+++ skeleton/avicenna/util.jl
@@ -1,52 +1,18 @@
 ####################################################################################################
 
-module XXX_ROOT_XXXCore
+module XXX_ROOT2_XXXCore
 
 ####################################################################################################
 
+# using
 
 ####################################################################################################
 
-export Input, Intermediate, Output
-export load_raw, transform, analyze
+# export
 
 ####################################################################################################
 
-struct Input
-  id::String
-  values::Vector{Float64}
-end
-
-struct Intermediate
-  source_id::String
-  transformed::Vector{Float64}
-  metadata::Dict
-end
-
-struct Output
-  summary::Dict
-  details::Vector
-end
-
-####################################################################################################
-
-function load_raw(id::String, raw_data::Vector{Float64})::Input
-  return Input(id, raw_data)
-end
-
-function transform(input::Input, params::Dict)::Intermediate
-  scale = get(params, "scale", 1.0)
-  transformed = [x * scale for x in input.values]
-  return Intermediate(input.id, transformed, Dict("params" => params))
-end
-
-function analyze(intermediate::Intermediate)::Output
-  summary = Dict(
-    "mean" => sum(intermediate.transformed) / length(intermediate.transformed),
-    "length" => length(intermediate.transformed),
-  )
-  return Output(summary, intermediate.transformed)
-end
+# function () end
 
 ####################################################################################################
 
