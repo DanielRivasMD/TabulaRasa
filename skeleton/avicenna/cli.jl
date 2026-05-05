fe39cb70c5c5d389dc9043af2f3f806ad68aeacd
Daniel Rivas <danielrivasmd@gmail.com>
2026-05-05 13:19:04 +0200

    update cli avicenna

--- skeleton/avicenna/cli.jl
+++ skeleton/avicenna/cli.jl
@@ -1,43 +1,38 @@
 ####################################################################################################
 
-module XXX_ROOT_XXXCLI
+module XXX_ROOT2_XXXCLI
 
 ####################################################################################################
 
 using ArgParse
-using Avicenna.Flow
-using ..XXX_ROOT_XXXFlow: demo
+using Avicenna.Flow: Cache, launch
+using ..XXX_ROOT2_XXXFlow: flow
+
+####################################################################################################
+
+export run
 
 ####################################################################################################
 
 function main(args = ARGS)
   s = ArgParseSettings()
   @add_arg_table! s begin
-    "--id"
-    required = true
-
-    "--scale"
-    arg_type = Float64
-    default = 1.0
-
     "--no-cache"
+    help = "Disable caching"
     action = :store_true
+    "--verbose"
+    help = "Enable verbose diagnostics"
+    action = :store_false
   end
 
-  parsed_args = parse_args(args, s)
-  demo_data = [1.0, 2.0, 3.0, 4.0, 5.0]
+  config = Dict()
 
-  config =
-    Dict("id" => parsed_args["id"], "data" => demo_data, "scale" => parsed_args["scale"])
+  cache = Cache("cache/_", !parsed["no-cache"])
+  result = launch(flow, config, cache = cache)
 
-  cache = Flow.Cache("cache/demo", !parsed_args["no-cache"])
-  result = Flow.run(demo, config, cache = cache)
-
-  println("Analysis complete:")
-  println("  Mean: ", result.stage_outputs["analyze"].summary["mean"])
-  println("  Cache hits: ", join(result.cache_hits, ", "))
-
-  return 0
+  if parsed["verbose"]
+  end
+  return result
 end
 
 ####################################################################################################
