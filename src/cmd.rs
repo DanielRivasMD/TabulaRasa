fe42656fed39aa8284e9bb228c1440f9635ef9be
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-16 21:53:44 +0300

    update cmd

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,9 +1,52 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub mod cobra;
-pub mod completion;
 pub mod deploy;
 pub mod etch;
-pub mod identity;
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub mod completion {
+
+    use anyhow::Result as anyResult;
+    use clap::{Command, CommandFactory};
+    use clap_complete::{generate, shells::*};
+    use std::io;
+
+    use crate::cli;
+
+    pub fn run(shell: cli::Shell) -> anyResult<()> {
+        let visible: Vec<_> = cli::Cli::command()
+            .get_subcommands()
+            .filter(|s| !s.is_hide_set())
+            .cloned()
+            .collect();
+
+        let mut cmd = Command::new(env!("CARGO_BIN_NAME")).subcommands(visible);
+
+        let name = cmd.get_name().to_string();
+
+        match shell {
+            cli::Shell::Bash => generate(Bash, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Zsh => generate(Zsh, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Fish => generate(Fish, &mut cmd, name, &mut io::stdout()),
+            cli::Shell::Powershell => generate(PowerShell, &mut cmd, name, &mut io::stdout()),
+        }
+        Ok(())
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+pub mod identity {
+    use anyhow::Result as anyResult;
+
+    pub const IDENTITY: &str = r#"Tabula Rasa"#;
+
+    pub fn run() -> anyResult<()> {
+        println!("{}", IDENTITY);
+        Ok(())
+    }
+}
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
