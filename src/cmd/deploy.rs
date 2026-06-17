bb58bff409ad376027f3b8f0ecaf6ee33a20e584
Daniel Rivas <danielrivasmd@gmail.com>
2026-06-17 13:19:38 +0300

    update deploy cmd

--- src/cmd/deploy.rs
+++ src/cmd/deploy.rs
@@ -7,6 +7,10 @@ use anyhow::Result as anyResult;
 use crate::cli;
 use crate::cmd::deploy;
 
+use crate::forge;
+use crate::skeleton;
+use crate::util;
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 pub fn run(lang: Option<&str>, sub: Option<cli::DeploySub>, verbose: bool) -> anyResult<()> {
@@ -27,20 +31,14 @@ pub fn run(lang: Option<&str>, sub: Option<cli::DeploySub>, verbose: bool) -> an
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 mod avicenna {
-    use anyhow::Result as anyResult;
-
-    use crate::forge;
-    use crate::skeleton;
-    use crate::util;
-
-    pub fn run(module: &str, verbose: bool) -> anyResult<()> {
-        let letter_upper = util::two_letter_from_module(module)?;
+    pub fn run(module: &str, verbose: bool) -> super::anyResult<()> {
+        let letter_upper = super::util::two_letter_from_module(module)?;
         let letter_lower = letter_upper.to_lowercase();
         let mod_lower = module.to_lowercase();
         let replacements = vec![
-            forge::Replacement::token("XXX_MODULE_LOWERCASE_XXX", &mod_lower),
-            forge::Replacement::token("XXX_ROOT2_UPPERCASE_XXX", &letter_upper),
-            forge::Replacement::token("XXX_ROOT2_LOWERCASE_XXX", &letter_lower),
+            issac::Replacement::token("XXX_MODULE_LOWERCASE_XXX", &mod_lower),
+            issac::Replacement::token("XXX_ROOT2_UPPERCASE_XXX", &letter_upper),
+            issac::Replacement::token("XXX_ROOT2_LOWERCASE_XXX", &letter_lower),
         ];
 
         let root_jl = format!("{module}.jl");
@@ -50,15 +48,15 @@ mod avicenna {
         let repl_jl = format!("{letter_lower}repl.jl");
 
         let targets: Vec<(&str, &str, &str)> = vec![
-            ("src", &root_jl, skeleton::avicenna::ROOT),
-            ("src/util", &util_jl, skeleton::avicenna::UTIL),
-            ("src/flow", &flow_jl, skeleton::avicenna::FLOW),
-            ("src/inter/cli", &cli_jl, skeleton::avicenna::CLI),
-            ("src/inter/repl", &repl_jl, skeleton::avicenna::REPL),
+            ("src", &root_jl, super::skeleton::avicenna::ROOT),
+            ("src/util", &util_jl, super::skeleton::avicenna::UTIL),
+            ("src/flow", &flow_jl, super::skeleton::avicenna::FLOW),
+            ("src/inter/cli", &cli_jl, super::skeleton::avicenna::CLI),
+            ("src/inter/repl", &repl_jl, super::skeleton::avicenna::REPL),
         ];
         for (subdir, filename, content) in &targets {
             let out_path = format!("{subdir}/{filename}");
-            forge::forge_files(&out_path, &[(*filename, *content)], &replacements, verbose)?;
+            super::forge::forge_files(&out_path, &[(*filename, *content)], &replacements, verbose)?;
         }
         Ok(())
     }
@@ -67,52 +65,40 @@ mod avicenna {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 mod just {
-    use anyhow::Result as anyResult;
-
-    use crate::forge;
-    use crate::skeleton;
-    use crate::util;
-
-    pub fn run(lang: Option<&str>, verbose: bool) -> anyResult<()> {
-        let repo = util::current_dir_name()?;
+    pub fn run(lang: Option<&str>, verbose: bool) -> super::anyResult<()> {
+        let repo = super::util::current_dir_name()?;
         let lower_repo = repo.to_lowercase();
-        let mut files = vec![("head.just", skeleton::just::HEAD)];
+        let mut files = vec![("head.just", super::skeleton::just::HEAD)];
 
         if let Some(lang_str) = lang {
             match lang_str {
-                "go" => files.push(("go.just", skeleton::just::GO)),
-                "rs" => files.push(("rs.just", skeleton::just::RS)),
+                "go" => files.push(("go.just", super::skeleton::just::GO)),
+                "rs" => files.push(("rs.just", super::skeleton::just::RS)),
                 _ => anyhow::bail!("unsupported language: {lang_str}"),
             }
         }
 
         let replacements = vec![
-            forge::Replacement::token("XXX_APP_XXX", &repo),
-            forge::Replacement::token("XXX_EXE_XXX", &lower_repo),
+            issac::Replacement::token("XXX_APP_XXX", &repo),
+            issac::Replacement::token("XXX_EXE_XXX", &lower_repo),
         ];
-        forge::forge_files(".justfile", &files, &replacements, verbose)
+        super::forge::forge_files(".justfile", &files, &replacements, verbose)
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 mod readme {
-    use anyhow::Result as anyResult;
-
-    use crate::forge;
-    use crate::skeleton;
-    use crate::util;
-
-    pub fn run(verbose: bool) -> anyResult<()> {
-        let repo = util::current_dir_name()?;
+    pub fn run(verbose: bool) -> super::anyResult<()> {
+        let repo = super::util::current_dir_name()?;
         let year = "2026";
         let replacements = vec![
-            forge::Replacement::token("XXX_REPO_XXX", &repo),
-            forge::Replacement::token("XXX_YEAR_XXX", year),
+            issac::Replacement::token("XXX_REPO_XXX", &repo),
+            issac::Replacement::token("XXX_YEAR_XXX", year),
         ];
-        forge::forge_files(
+        super::forge::forge_files(
             "README.md",
-            &[("readme.md", skeleton::readme::MD)],
+            &[("readme.md", super::skeleton::readme::MD)],
             &replacements,
             verbose,
         )
@@ -122,13 +108,8 @@ mod readme {
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 
 mod todor {
-    use anyhow::Result as anyResult;
-
-    use crate::forge;
-    use crate::skeleton;
-
-    pub fn run(verbose: bool) -> anyResult<()> {
-        forge::forge_files(".todor", &[("todor", skeleton::todor::TODOR)], &[], verbose)
+    pub fn run(verbose: bool) -> super::anyResult<()> {
+        super::forge::forge_files(".todor", &[("todor", super::skeleton::todor::TODOR)], &[], verbose)
     }
 }
 
